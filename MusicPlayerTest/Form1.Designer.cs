fed25f50024f1ecc69856a940a4ac1f4f07d7950
Junior Sousa <juniorjblp@gmail.com>
2021-12-14 08:17:46 -0300

    Editado tamanho o player e da imagem dp album, Cores da basrra superior e inferir

--- MusicPlayerTest/Form1.Designer.cs
+++ MusicPlayerTest/Form1.Designer.cs
@@ -36,20 +36,15 @@ namespace MusicPlayerTest
             this.Pnl_Title = new System.Windows.Forms.Panel();
             this.Pnl_Botton = new System.Windows.Forms.Panel();
             this.Pnl_Center = new System.Windows.Forms.Panel();
-            this.Txt_SongName = new System.Windows.Forms.TextBox();
-            this.iconButton1 = new FontAwesome.Sharp.IconButton();
-            this.Txt_AtistName = new System.Windows.Forms.TextBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.Lbl_AlbumName = new System.Windows.Forms.Label();
             this.Lbl_ArtistName = new System.Windows.Forms.Label();
             this.Lbl_SongName = new System.Windows.Forms.Label();
             this.Pic_CoverAlbum = new System.Windows.Forms.PictureBox();
-            this.Pic_BackGround = new System.Windows.Forms.PictureBox();
-            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.Pnl_Title.SuspendLayout();
             this.Pnl_Center.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Pic_CoverAlbum)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Pic_BackGround)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_CoverAlbum)).BeginInit();
             this.SuspendLayout();
             // 
             // Btn_Close
@@ -102,7 +97,7 @@ namespace MusicPlayerTest
             // 
             // Pnl_Title
             // 
-            this.Pnl_Title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(47)))), ((int)(((byte)(60)))));
+            this.Pnl_Title.BackColor = System.Drawing.SystemColors.HotTrack;
             this.Pnl_Title.Controls.Add(this.Btn_Maximize);
             this.Pnl_Title.Controls.Add(this.Btn_Minimize);
             this.Pnl_Title.Controls.Add(this.Btn_Close);
@@ -115,7 +110,7 @@ namespace MusicPlayerTest
             // 
             // Pnl_Botton
             // 
-            this.Pnl_Botton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(47)))), ((int)(((byte)(60)))));
+            this.Pnl_Botton.BackColor = System.Drawing.SystemColors.HotTrack;
             this.Pnl_Botton.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.Pnl_Botton.Location = new System.Drawing.Point(0, 580);
             this.Pnl_Botton.Name = "Pnl_Botton";
@@ -125,70 +120,35 @@ namespace MusicPlayerTest
             // Pnl_Center
             // 
             this.Pnl_Center.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(55)))), ((int)(((byte)(71)))));
-            this.Pnl_Center.Controls.Add(this.axWindowsMediaPlayer1);
-            this.Pnl_Center.Controls.Add(this.Txt_SongName);
-            this.Pnl_Center.Controls.Add(this.iconButton1);
-            this.Pnl_Center.Controls.Add(this.Txt_AtistName);
             this.Pnl_Center.Controls.Add(this.Lbl_AlbumName);
             this.Pnl_Center.Controls.Add(this.Lbl_ArtistName);
             this.Pnl_Center.Controls.Add(this.Lbl_SongName);
             this.Pnl_Center.Controls.Add(this.Pic_CoverAlbum);
-            this.Pnl_Center.Controls.Add(this.Pic_BackGround);
+            this.Pnl_Center.Controls.Add(this.axWindowsMediaPlayer1);
             this.Pnl_Center.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Pnl_Center.Location = new System.Drawing.Point(0, 35);
             this.Pnl_Center.Name = "Pnl_Center";
             this.Pnl_Center.Size = new System.Drawing.Size(800, 545);
             this.Pnl_Center.TabIndex = 3;
             // 
-            // Txt_SongName
+            // axWindowsMediaPlayer1
             // 
-            this.Txt_SongName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.Txt_SongName.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Txt_SongName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Txt_SongName.ForeColor = System.Drawing.Color.White;
-            this.Txt_SongName.Location = new System.Drawing.Point(225, 230);
-            this.Txt_SongName.Name = "Txt_SongName";
-            this.Txt_SongName.Size = new System.Drawing.Size(219, 22);
-            this.Txt_SongName.TabIndex = 1;
-            this.Txt_SongName.Visible = false;
-            this.Txt_SongName.Enter += new System.EventHandler(this.Txt_SongName_Enter);
-            // 
-            // iconButton1
-            // 
-            this.iconButton1.FlatAppearance.BorderSize = 0;
-            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.Search;
-            this.iconButton1.IconColor = System.Drawing.Color.White;
-            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButton1.IconSize = 57;
-            this.iconButton1.Location = new System.Drawing.Point(477, 195);
-            this.iconButton1.Name = "iconButton1";
-            this.iconButton1.Size = new System.Drawing.Size(57, 57);
-            this.iconButton1.TabIndex = 2;
-            this.iconButton1.UseVisualStyleBackColor = true;
-            this.iconButton1.Visible = false;
-            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
-            // 
-            // Txt_AtistName
-            // 
-            this.Txt_AtistName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.Txt_AtistName.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Txt_AtistName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Txt_AtistName.ForeColor = System.Drawing.Color.White;
-            this.Txt_AtistName.Location = new System.Drawing.Point(225, 190);
-            this.Txt_AtistName.Name = "Txt_AtistName";
-            this.Txt_AtistName.Size = new System.Drawing.Size(219, 24);
-            this.Txt_AtistName.TabIndex = 0;
-            this.Txt_AtistName.Visible = false;
-            this.Txt_AtistName.Enter += new System.EventHandler(this.Txt_AtistName_Enter);
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(800, 545);
+            this.axWindowsMediaPlayer1.TabIndex = 5;
             // 
             // Lbl_AlbumName
             // 
             this.Lbl_AlbumName.AutoSize = true;
+            this.Lbl_AlbumName.BackColor = System.Drawing.Color.Black;
             this.Lbl_AlbumName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Lbl_AlbumName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Lbl_AlbumName.ForeColor = System.Drawing.SystemColors.Control;
-            this.Lbl_AlbumName.Location = new System.Drawing.Point(166, 111);
+            this.Lbl_AlbumName.Location = new System.Drawing.Point(207, 107);
             this.Lbl_AlbumName.Name = "Lbl_AlbumName";
             this.Lbl_AlbumName.Size = new System.Drawing.Size(46, 18);
             this.Lbl_AlbumName.TabIndex = 3;
@@ -197,10 +157,11 @@ namespace MusicPlayerTest
             // Lbl_ArtistName
             // 
             this.Lbl_ArtistName.AutoSize = true;
+            this.Lbl_ArtistName.BackColor = System.Drawing.Color.Black;
             this.Lbl_ArtistName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Lbl_ArtistName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Lbl_ArtistName.ForeColor = System.Drawing.SystemColors.Control;
-            this.Lbl_ArtistName.Location = new System.Drawing.Point(166, 72);
+            this.Lbl_ArtistName.Location = new System.Drawing.Point(207, 68);
             this.Lbl_ArtistName.Name = "Lbl_ArtistName";
             this.Lbl_ArtistName.Size = new System.Drawing.Size(46, 18);
             this.Lbl_ArtistName.TabIndex = 2;
@@ -209,10 +170,11 @@ namespace MusicPlayerTest
             // Lbl_SongName
             // 
             this.Lbl_SongName.AutoSize = true;
+            this.Lbl_SongName.BackColor = System.Drawing.Color.Black;
             this.Lbl_SongName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Lbl_SongName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Lbl_SongName.ForeColor = System.Drawing.SystemColors.Control;
-            this.Lbl_SongName.Location = new System.Drawing.Point(166, 32);
+            this.Lbl_SongName.Location = new System.Drawing.Point(207, 28);
             this.Lbl_SongName.Name = "Lbl_SongName";
             this.Lbl_SongName.Size = new System.Drawing.Size(57, 20);
             this.Lbl_SongName.TabIndex = 1;
@@ -223,28 +185,10 @@ namespace MusicPlayerTest
             this.Pic_CoverAlbum.Image = global::MusicPlayerTest.Properties.Resources.cover;
             this.Pic_CoverAlbum.Location = new System.Drawing.Point(21, 17);
             this.Pic_CoverAlbum.Name = "Pic_CoverAlbum";
-            this.Pic_CoverAlbum.Size = new System.Drawing.Size(120, 120);
+            this.Pic_CoverAlbum.Size = new System.Drawing.Size(180, 180);
             this.Pic_CoverAlbum.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Pic_CoverAlbum.TabIndex = 0;
             this.Pic_CoverAlbum.TabStop = false;
-            // 
-            // Pic_BackGround
-            // 
-            this.Pic_BackGround.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Pic_BackGround.Location = new System.Drawing.Point(0, 0);
-            this.Pic_BackGround.Name = "Pic_BackGround";
-            this.Pic_BackGround.Size = new System.Drawing.Size(800, 545);
-            this.Pic_BackGround.TabIndex = 4;
-            this.Pic_BackGround.TabStop = false;
-            // 
-            // axWindowsMediaPlayer1
-            // 
-            this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(3, 258);
-            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
-            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(797, 111);
-            this.axWindowsMediaPlayer1.TabIndex = 5;
             // 
             // Form_Principal
             // 
@@ -262,9 +206,8 @@ namespace MusicPlayerTest
             this.Pnl_Title.ResumeLayout(false);
             this.Pnl_Center.ResumeLayout(false);
             this.Pnl_Center.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Pic_CoverAlbum)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Pic_BackGround)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_CoverAlbum)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -280,10 +223,6 @@ namespace MusicPlayerTest
         private System.Windows.Forms.PictureBox Pic_CoverAlbum;
         private System.Windows.Forms.Label Lbl_AlbumName;
         private System.Windows.Forms.Label Lbl_ArtistName;
-        private System.Windows.Forms.TextBox Txt_AtistName;
-        private FontAwesome.Sharp.IconButton iconButton1;
-        private System.Windows.Forms.TextBox Txt_SongName;
-        private System.Windows.Forms.PictureBox Pic_BackGround;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
--- MusicPlayerTest/Form1.cs
+++ MusicPlayerTest/Form1.cs
@@ -1,21 +1,11 @@
 ﻿using MusicPlayerTest.Uteis;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Net;
-using System.IO;
-using System.Windows.Media;
-using System.Media;
-using System.Timers;
+
 
 namespace MusicPlayerTest
 {
@@ -37,10 +27,6 @@ namespace MusicPlayerTest
             Lbl_SongName.Text = "...";
             Lbl_ArtistName.Text = "...";
             Lbl_AlbumName.Text = "...";
-
-            Txt_AtistName.Text = "Artist Name";
-            Txt_SongName.Text = "Song Name";
-
         }
         private void Form_Principal_Load(object sender, EventArgs e)
         {
@@ -73,104 +59,7 @@ namespace MusicPlayerTest
             ReleaseCapture();
             SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
-
-        private void Txt_ArtistName_Leave(object sender, EventArgs e)
-        {
-
-        }
-        private void iconButton1_Click(object sender, EventArgs e)
-        {
-            //var ArtistName = Txt_AtistName.Text;
-            //var SongName = Txt_SongName.Text;
-
-            //var jsonResponse = iTunesResult.iTunesReturn(ArtistName, SongName);
-
-            //Music.Root myDeserializedClass = JsonConvert.DeserializeObject<Music.Root>(jsonResponse);
-
-            //if (myDeserializedClass.results.Count == 0)
-            //{
-            //    MessageBox.Show("Nenhum item encontrado");
-            //    Txt_AtistName.Clear();
-            //    Txt_AtistName.Focus();
-            //    Lbl_AlbumName.Text = "...";
-            //    Lbl_SongName.Text = "...";
-            //    Lbl_ArtistName.Text = "...";
-            //    Pic_CoverAlbum.Image = Properties.Resources.cover;
-            //}
-            //else
-            //{
-            //    foreach (var item in myDeserializedClass.results)
-            //    {
-            //        var request = WebRequest.Create(item.artworkUrl100.Replace("100x100", "350x350"));
-            //        using (var response = request.GetResponse())
-            //        using (var stream = response.GetResponseStream())
-            //        {
-            //            Pic_CoverAlbum.Image = Image.FromStream(stream);
-
-            //        }
-
-            //        var requestBack = WebRequest.Create(item.artworkUrl100.Replace("100x100", "1500x1500"));
-            //        using (var response = requestBack.GetResponse())
-            //        using (var stream = response.GetResponseStream())
-            //        {
-            //            Pic_BackGround.Image = Image.FromStream(stream);
-            //            Pic_BackGround.SizeMode = PictureBoxSizeMode.StretchImage;
-            //        }
-
-            //            Lbl_SongName.Text = item.trackName;
-            //        Lbl_ArtistName.Text = item.artistName;
-            //        Lbl_AlbumName.Text = item.collectionName;
-            //    }
-            //}
-
-
-
-            //var StreamUrl = "brasilhits.com";
-
-            //var jsonResponse = StreamingReturn.iTunesReturn(StreamUrl);
-
-            //Streaming.Root myStreamReturn = JsonConvert.DeserializeObject<Streaming.Root>(jsonResponse);
-
-            //foreach (var item in myStreamReturn.icestats.source)
-            //{
-            //    var musicReturn = iTunesResult.iTunesReturn(item.title, "");
-
-            //    Music.Root myDeserializedClass = JsonConvert.DeserializeObject<Music.Root>(musicReturn);
-
-            //    foreach (var music in myDeserializedClass.results)
-            //    {
-            //        Lbl_SongName.Text = music.trackName;
-            //        Lbl_ArtistName.Text = music.artistName;
-            //        Lbl_AlbumName.Text = music.collectionName;
-            //        var request = WebRequest.Create(music.artworkUrl100.Replace("100x100", "350x350"));
-            //        using (var response = request.GetResponse())
-            //        using (var stream = response.GetResponseStream())
-            //        {
-            //            Pic_CoverAlbum.Image = Image.FromStream(stream);
-
-            //        }
-
-            //    }
-            //}
-        }
-
-
-        private void Txt_SongName_Enter(object sender, EventArgs e)
-        {
-            if (Txt_SongName.Text == "Song Name")
-            {
-                Txt_SongName.Text = "";
-            }
-        }
-
-        private void Txt_AtistName_Enter(object sender, EventArgs e)
-        {
-            if (Txt_AtistName.Text == "Artist Name")
-            {
-                Txt_AtistName.Text = "";
-            }
-        }
-        private void SongInfoLoad()
+                private void SongInfoLoad()
         {
             var StreamUrl = "brasilhits.com";
 
